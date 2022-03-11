14f8477c26db70c0c0f012e2e8712b1decde7ea2
A E <aelmollaz@gmail.com>
2022-03-11 12:26:33 -0500

    Add graphql schema & resolvers

---
+++ src/schema.js
@@ -0,0 +1,26 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql
+`
+type Student {
+  id: ID!
+  email: String!
+  fullName: String!
+  dept: String
+  enrolled: Boolean
+}
+
+type Query {
+  enrollment: [Student!]
+  students: [Student!]!
+  student(id: ID!): Student
+}
+
+type Mutation {
+  registerStudent(email: String!, fullName: String!, dept: String): Student!
+  enroll(id: ID!): Student
+}
+`
+module.exports = {
+  typeDefs,
+}
